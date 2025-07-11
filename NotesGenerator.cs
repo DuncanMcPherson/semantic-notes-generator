a871b0d430effb53168b0540c9f64ecce191bb84
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-07-10 22:44:27 -0600

    feat: Add support for sorting and grouping commits by type

--- NotesGenerator.cs
+++ NotesGenerator.cs
@@ -1,8 +1,65 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using SemanticRelease.Abstractions;
 
 namespace SemanticRelease.NotesGenerator
 {
-    public class NotesGenerator
+    public class NotesGenerator : ISemanticPlugin
     {
+        private readonly Dictionary<string, string> _commitTypes = new Dictionary<string, string>
+        {
+            {"!", "Breaking Changes"},
+            {"feat", "Features"},
+            {"fix", "Bug Fixes"},
+            {"docs", "Documentation"},
+            {"refactor", "Code Refactoring"},
+            {"perf", "Performance Improvements"},
+            {"test", "Tests"},
+            {"chore", "Chores"},
+        };
+        public void Register(SemanticLifecycle lifecycle)
+        {
+            lifecycle.OnGenerateNotes(GenerateNotesAsync);
+        }
+        
+        private async Task GenerateNotesAsync(ReleaseContext context)
+        {
+            if (!(context.PluginData["commits"] is List<Commit> commits) || commits.Count == 0)
+            {
+                return;
+            }
+
+            var typedCommits = new Dictionary<string, List<Commit>>();
+
+            foreach (var commit in commits)
+            {
+                var containsBreaking = commit.Message.Contains("BREAKING CHANGE") || commit.Message.Split('\n')[0].Split(':')[0].EndsWith('!');
+                if (containsBreaking)
+                {
+                    if (!typedCommits.TryGetValue("Breaking Changes", out var breakingChangeList))
+                    {
+                        breakingChangeList = new List<Commit>();
+                        typedCommits.Add("Breaking Changes", breakingChangeList);   
+                    }
+                    breakingChangeList.Add(commit);
+                    continue;
+                }
+                var type = commit.Message.Split('\n')[0].Split(':')[0].ToLower();
+                if (type.Contains('('))
+                    type = type.Split('(')[0];
+                var sectionHeader = _commitTypes.GetValueOrDefault(type);
+                if (sectionHeader == null)
+                    continue;
+                if (!typedCommits.TryGetValue(sectionHeader, out var list))
+                {
+                    list = new List<Commit>();
+                    typedCommits.Add(sectionHeader, list);
+                }
+                list.Add(commit);
+            }
+            
+            
+        }
     }
 }
